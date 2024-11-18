3344c89cdf1b68f498002277b24a5a2037630b29
clement lin <clementlinn@outlook.fr>
2024-11-18 20:55:06 +0100

    Dernier commit

--- src/pages/home/Article.tsx
+++ src/pages/home/Article.tsx
@@ -1,34 +1,9 @@
-
-//  <index>
-//      <Article />
-//  </index>
-// props = propriétés
-// objet: { isGreen: true }
-// props.isGreen
-// <Article monProp="test" />
-// Article = props
-// <Article isGreen />
-// <Article isGreen={true} />
-// <Article handleCardClick={() => 42}
-// props = { prop1, prop2, prop3 }
-// <Article prop1="" prop2="" prop3="" />
-// <Article prop1={ prop1, prop2, prop3 } />
-
 import { Article as ArticleType } from "./api"
 
 interface ArticleProps {
  index: number
  handleCardClick: (article: ArticleType) => void
 }
-// const ObjectClem = { couleur: 'rouge', voiture: 'BM', ordinateur: 'MAC', valise: { stylo: 'bic', instument: 'flutte' } }
-// afficher(monObjet.couleur)
-// afficher(monObjet.voiture)
-
-// const { couleur, voiture, ordinateur, { console } } = { couleur: 'rouge', voiture: 'BM', ordinateur: 'MAC' }
-// const { couleur, voiture, ordinateur, } = LesObjectClem()
-// const monObjet = recevoirMonObjet()
-// afficher(monObjet.voiture)
-// Destructuration VVVVVV
 const Article = ({ article, index, handleCardClick }: { article: ArticleType } & ArticleProps) => {
 
   return (
--- src/pages/home/api.ts
+++ src/pages/home/api.ts
@@ -15,22 +15,16 @@ export interface Article {
 export interface HomePageArticlesResponse {
   status: string
   totalResults: number
-  // Articles peut etre undifined si il y a une erreur
   articles?: Article[]
 }
 
-// select react
-// how to make a controlled select react
 export type HomePageFilter = 'none' | 'business' | 'entertainment' | 'health' | 'science' | 'sports' | 'technology'
 
-// Promise = possibilité que l'url ne revoie pas de contenu (TS)
-// filter = 'toto', 'tata' '3', 'jdofrnjcodl' null, 43, {}
 export async function fetchHomePageArticles(filter: HomePageFilter): Promise<HomePageArticlesResponse> {
   const api_key = import.meta.env.VITE_KEY_API
   const url = "https://newsapi.org/v2/top-headlines?";
   const params = new URLSearchParams()
 
-  // Url params used
   params.append('apiKey', api_key)
   params.append('country', 'us')
   if (filter !== 'none')
@@ -53,8 +47,7 @@ export async function fetchHomePageArticles(filter: HomePageFilter): Promise<Hom
       }
       return article
     })
-
-    // Spread operator VVV (...)
+    
     return { ...json, articles }
   } catch (error: any) {
     console.error(error.message);
